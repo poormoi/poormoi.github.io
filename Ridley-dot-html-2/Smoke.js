fbc6a032fd005e1b627f919f2808f1672a4137b8
ridleyisalive <ridley.tankersley@gmail.com>
2018-02-15 15:47:55 -0600

    domain renewal updates
    - added to projects page
    - edited cv
    - fixed broken links
    - updated contact email address
    - included Ridley.html as an integrated project

---
+++ Ridley-dot-html-2/Smoke.js
@@ -0,0 +1,12 @@
+// Smoke me up
+
+var x = "/you have smoked weed";
+var y = "/game over - Thank You For Playing :  -  )";
+function smoke() {
+ document.getElementById("print").innerHTML = x;
+ document.getElementById("print2").innerHTML = y;
+}
+
+function bad() {
+ document.getElementsByClassName("whole").innerHTML = "dsf";
+}
---
+++ Ridley-html-files/WeedGameTM/Smoke.js
@@ -0,0 +1,8 @@
+// Smoke me up
+
+var x = "/you have smoked weed";
+var y = "/game over - Thank You For Playing :  -  )";
+function smoke() {
+ document.getElementById("print").innerHTML = x;
+ document.getElementById("print2").innerHTML = y;
+}
